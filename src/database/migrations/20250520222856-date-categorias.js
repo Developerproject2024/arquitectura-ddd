1bd1bce960861a5f98443ec3c6197172d547c429
farango@gmail.com <farango0728@gmail.com>
2025-05-20 17:37:35 -0500

    se crean migraciones

--- src/database/migrations/20250520222856-date-categorias.js
+++ src/database/migrations/20250520222856-date-categorias.js
@@ -2,7 +2,7 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up (queryInterface, Sequelize) {
     await queryInterface.createTable('categorias', {
       id: {
         type: Sequelize.INTEGER,
@@ -14,12 +14,12 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      created_at: {
+      createdAt: {
         type: Sequelize.DATE,
         allowNull: false,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
-      updated_at: {
+      updatedAt: {
         type: Sequelize.DATE,
         allowNull: false,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
@@ -27,7 +27,7 @@ module.exports = {
     });
   },
 
-  async down(queryInterface, Sequelize) {
+  async down (queryInterface, Sequelize) {
     await queryInterface.dropTable('categorias');
   }
 };
